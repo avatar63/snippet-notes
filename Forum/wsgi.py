d2b85e8419a1872ef6b3d6fef19c243af48bf9e1
avatar63 <2002.dhruvgarg@gmail.com>
2022-10-20 10:21:27 +0530

    Update wsgi.py

--- Forum/wsgi.py
+++ Forum/wsgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "xxxx.settings")
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Forum.settings')
-
+from whitenoise.django import DjangoWhiteNoise
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
+
